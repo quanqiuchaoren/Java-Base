998dda994e77e8dfe787aa7935191d6b26048f3f
quanqiuchaoren <quanqiuchaoren@gmail.com>
2023-06-18 12:19:17 +0800

    使用FileChannel读取文件

---
+++ src/test/java/com/lhy/java/base/nio_1_0/FileChannelTest.java
@@ -0,0 +1,39 @@
+package com.lhy.java.base.nio_1_0;
+
+import com.lhy.java.base.nio_1_0.util.FileUtil;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+
+public class FileChannelTest {
+    @Test
+    void hello2() throws IOException {
+        /*
+        文件中的内容为：
+        first line
+        second line
+         */
+        String filePathStr = FileUtil.getFilePathStr("fileChannel.txt");
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(filePathStr, "rw");
+        ) {
+            FileChannel fileChannel = randomAccessFile.getChannel();
+            ByteBuffer byteBuffer = ByteBuffer.allocate(8);
+            while (fileChannel.read(byteBuffer) != -1) {
+                byteBuffer.flip(); // 读写切换时，要调用flip()方法。
+                System.out.println(StandardCharsets.UTF_8.decode(byteBuffer));
+                byteBuffer.clear(); // 需要清除之前读入的数据，否则下一次获取byteBuffer中的数据时，一直是第一次读入的数据。
+            }
+        }
+        /*
+         * 输出：
+            first li
+            ne
+            seco
+            nd line
+         */
+    }
+}
